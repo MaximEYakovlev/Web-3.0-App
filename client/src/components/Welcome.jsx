70d5225f13a3b80e5801e7f6935328eb8800c71d
Maxim Yakovlev <it.trainee@yandex.ru>
2022-06-03 23:04:08 +0300

    client / Welcome.jsx: added an image of a plastic bank card

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -36,6 +36,11 @@ const Welcome = () => {
             <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
           </div>
         </div>
+        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+          <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
+            
+          </div>
+        </div>
       </div>
     </div>
   );
