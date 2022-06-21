2989712588a24bfbda30b7f924d7e963b3fe8f67
Maxim Yakovlev <it.trainee@yandex.ru>
2022-06-21 23:07:05 +0300

    client / context / TransactionContext.jsx: added the checkIfTransactionsExist function

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -55,6 +55,19 @@ export const TransactionProvider = ({ children }) => {
     }
   };
 
+  const checkIfTransactionsExist = async () => {
+    try {
+      const transactionContract = getEthereumContract();
+      const transactionCount = await transactionContract.getTransactionCount();
+
+      window.localStorage.setItem("transactionCount", transactionCount);
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("No ethereum object.");
+    }
+  };
+
   const connectWallet = async () => {
     try {
       if (!ethereum) return alert("Please install MetaMask.");
@@ -116,6 +129,7 @@ export const TransactionProvider = ({ children }) => {
 
   useEffect(() => {
     checkIfWalletIsConnected();
+    checkIfTransactionsExist();
   }, []);
 
   return (
